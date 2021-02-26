7649b9529d4e57b0ed4c740fc67ddac2b64a6191
gilzoide <gilzoide@gmail.com>
2021-02-26 19:07:28 -0300

    Only drag lights on left mouse button

--- src/Editor/Visualizers/DraggableNode2D.gd
+++ src/Editor/Visualizers/DraggableNode2D.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 export(float) var radius: float = 25
+
 onready var radius_squared = radius * radius
 var dragging = false
+onready var sprite = $Sprite
 
-func is_mouse_near() -> bool:
-	var mouse_pos = get_local_mouse_position()
-	return mouse_pos.length_squared() <= radius_squared
+func is_mouse_over() -> bool:
+	return sprite.get_rect().has_point(sprite.get_local_mouse_position())
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouse and event.is_pressed() and is_mouse_near():
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed() and is_mouse_over():
 		dragging = true
 	elif event is InputEventMouseButton and not event.is_pressed():
 		dragging = false
