746b06385f3744c9c7fd9cc080e2e0fa1ed275a7
gilzoide <gilzoide@gmail.com>
2021-02-27 11:25:52 -0300

    Add separator to textures context menu, move visualizer options context menu to top left

--- src/editor/TextureSlot.gd
+++ src/editor/TextureSlot.gd
@@ -24,6 +24,7 @@ func _ready() -> void:
 	texture_rect.texture = MapTypes.map_texture(type)
 	menu_popup.add_check_item("Filter texture", TOGGLE_FILTER)
 	update_filter_check_item()
+	menu_popup.add_separator()
 	menu_popup.add_item("Load...", LOAD_IMAGE)
 	menu_popup.add_item("Save as...", SAVE_IMAGE_AS)
 	menu_popup.hide_on_checkable_item_selection = false
