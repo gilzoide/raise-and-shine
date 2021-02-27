d3e6548b192213a7eab61ec8bdde26a99f1f7742
gilzoide <gilzoide@gmail.com>
2021-02-27 05:37:28 -0300

    Add checkbox for toggling texture filtering

--- src/editor/TextureSlot.gd
+++ src/editor/TextureSlot.gd
@@ -7,6 +7,7 @@ enum Type {
 }
 
 enum {
+	TOGGLE_FILTER,
 	LOAD_IMAGE,
 	SAVE_IMAGE_AS,
 }
@@ -15,18 +16,23 @@ export(Type) var type
 export(Resource) var project = preload("res://editor/project/ActiveEditorProject.tres")
 
 onready var title_menu_button = $Title
+onready var menu_popup = title_menu_button.get_popup()
 onready var texture_rect = $TextureRect
 
 func _ready() -> void:
 	title_menu_button.text = MapTypes.map_name(type)
 	texture_rect.texture = MapTypes.map_texture(type)
-	var menu_popup = title_menu_button.get_popup()
+	menu_popup.add_check_item("Filter texture", TOGGLE_FILTER)
+	menu_popup.set_item_checked(TOGGLE_FILTER, texture_rect.texture.flags & Texture.FLAG_FILTER)
 	menu_popup.add_item("Load...", LOAD_IMAGE)
 	menu_popup.add_item("Save as...", SAVE_IMAGE_AS)
 	menu_popup.connect("id_pressed", self, "_on_menu_id_pressed")
 
 func _on_menu_id_pressed(id: int) -> void:
-	if id == LOAD_IMAGE:
+	if id == TOGGLE_FILTER:
+		texture_rect.texture.flags ^= Texture.FLAG_FILTER
+		menu_popup.set_item_checked(TOGGLE_FILTER, texture_rect.texture.flags & Texture.FLAG_FILTER)
+	elif id == LOAD_IMAGE:
 		project.load_image_dialog(type)
 	elif id == SAVE_IMAGE_AS:
 		project.save_image_dialog(type)
