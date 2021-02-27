0d9aabdb15c2227e5ef017bb7b65c2d06593ddc6
gilzoide <gilzoide@gmail.com>
2021-02-27 04:49:25 -0300

    Fix quit shortcut, remove unused "About" menu

--- src/mainUI/FileMenuButton.gd
+++ src/mainUI/FileMenuButton.gd
@@ -11,7 +11,7 @@ enum {
 func _ready() -> void:
 	var popup = get_popup()
 	popup.add_item("Quit", QUIT)
-	popup.set_item_shortcut(QUIT, load("res://mainUI/FileMenuButton/Quit_shortcut.tres"))
+	popup.set_item_shortcut(QUIT, load("res://shortcuts/Quit_shortcut.tres"))
 	popup.connect("id_pressed", self, "_on_item_pressed")
 
 func _on_item_pressed(id: int) -> void:
