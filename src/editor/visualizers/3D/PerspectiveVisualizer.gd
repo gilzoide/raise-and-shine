efca1d41213ec0b46bab04c3bc6f886fc25c195d
gilzoide <gilzoide@gmail.com>
2021-02-28 14:12:43 -0300

    Update 3D mouse orbit input to speed up on shift

--- src/editor/visualizers/3D/PerspectiveVisualizer.gd
+++ src/editor/visualizers/3D/PerspectiveVisualizer.gd
@@ -7,7 +7,6 @@ export(float) var mouse_speed: float = 0.01
 onready var camera: Camera = $ViewportContainer/Viewport/Camera
 onready var camera_initial_position: Vector3 = camera.translation
 var dragging: bool = false
-var euler_angles: Vector3 = Vector3.ZERO
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and not event.is_echo() \
@@ -16,7 +15,8 @@ func _gui_input(event: InputEvent) -> void:
 	elif event is InputEventMouseButton and not event.is_pressed():
 		dragging = false
 	if dragging and event is InputEventMouseMotion:
-		update_orbit_camera(event.relative * mouse_speed, 0)
+		var factor = (faster_factor if Input.is_action_pressed("visualizer_3d_faster") else 1.0) * mouse_speed
+		update_orbit_camera(event.relative * factor, 0)
 
 func _process(delta: float) -> void:
 	if has_focus():
