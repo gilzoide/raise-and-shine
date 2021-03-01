75dd98f982769aa0542d9efad02cfe043509d9b3
gilzoide <gilzoide@gmail.com>
2021-02-28 22:42:29 -0300

    Cache drawn rectangle

--- src/editor/RayCastTextureRect.gd
+++ src/editor/RayCastTextureRect.gd
@@ -14,23 +14,28 @@ class ScaledRect:
 		scale = _scale
 
 var last_position: Vector2 = INVALID_POSITION
+var drawn_rect: ScaledRect
+
+func _ready() -> void:
+	update_drawn_rect()
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_MOUSE_EXIT:
 		emit_signal("mouse_exited_texture")
+	elif what == NOTIFICATION_RESIZED:
+		update_drawn_rect()
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var scaled_rect = get_drawn_rect()
-		if scaled_rect.rect.has_point(event.position):
-			last_position = ((event.position - scaled_rect.rect.position) / scaled_rect.scale).floor()
+		if drawn_rect.rect.has_point(event.position):
+			last_position = ((event.position - drawn_rect.rect.position) / drawn_rect.scale).floor()
 			emit_signal("position_hovered", last_position)
 		else:
 			if not last_position.is_equal_approx(INVALID_POSITION):
 				emit_signal("mouse_exited_texture")
 			last_position = INVALID_POSITION
 
-func get_drawn_rect() -> ScaledRect:
+func update_drawn_rect() -> void:
 	# Adapted from TextureRect draw code to include `scale` result
 	# Ref: https://github.com/godotengine/godot/blob/7961a1dea3e7ce8c4e7197a0000e35ab31e9ff2e/scene/gui/texture_rect.cpp#L66-L81
 	var texture_size = texture.get_size()
@@ -46,4 +51,4 @@ func get_drawn_rect() -> ScaledRect:
 	
 	var offset = Vector2((size.x - tex_width) * 0.5, (size.y - tex_height) * 0.5)
 	var rect = Rect2(offset.x, offset.y, tex_width, tex_height)
-	return ScaledRect.new(rect, scale)
+	drawn_rect = ScaledRect.new(rect, scale)
