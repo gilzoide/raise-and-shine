b71ca7298b59688ad7a130a23ed3660700afc0a3
gilzoide <gilzoide@gmail.com>
2021-03-01 18:51:13 -0300

    Add light editor with an Autoload popup

--- src/editor/visualizers/LightPoint.gd
+++ src/editor/visualizers/LightPoint.gd
@@ -1,10 +1,16 @@
 extends CollisionObject
 
+onready var light = $SpotLight
+onready var sphere = $SphereMesh
 var dragging: bool = false
 
 func _input_event(camera: Object, event: InputEvent, click_position: Vector3, click_normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		dragging = event.is_pressed()
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			dragging = event.is_pressed()
+		elif event.button_index == BUTTON_RIGHT and event.is_pressed() and not event.is_echo():
+			var position = get_tree().root.get_viewport().get_mouse_position()
+			LightEditorPopup.popup_at(position, light.light_color, funcref(self, "set_light_color"))
 	elif dragging and event is InputEventMouseMotion:
 		var position_in_xy = Plane.PLANE_XY.intersects_ray(
 			camera.project_ray_origin(event.position),
@@ -12,3 +18,7 @@ func _input_event(camera: Object, event: InputEvent, click_position: Vector3, cl
 		)
 		if position_in_xy:
 			translation = position_in_xy
+
+func set_light_color(color: Color) -> void:
+	light.light_color = color
+	sphere.material_override.albedo_color = color
