6d78942120c77100c3e1471014d03d6f7ba9440b
gilzoide <gilzoide@gmail.com>
2021-03-02 16:20:54 -0300

    Avoid showing "Quit" button on HTML5

--- src/mainUI/FileMenuButton.gd
+++ src/mainUI/FileMenuButton.gd
@@ -6,8 +6,9 @@ enum {
 
 func _ready() -> void:
 	var popup = get_popup()
-	popup.add_item("Quit", QUIT)
-	popup.set_item_shortcut(QUIT, load("res://shortcuts/Quit_shortcut.tres"))
+	if OS.get_name() != "HTML5":
+		popup.add_item("Quit", QUIT)
+		popup.set_item_shortcut(QUIT, load("res://shortcuts/Quit_shortcut.tres"))
 	popup.connect("id_pressed", self, "_on_item_pressed")
 
 func _on_item_pressed(id: int) -> void:
