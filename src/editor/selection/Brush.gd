9ba75d5e989e9600baa4e38548adfe2578cf9e59
gilzoide <gilzoide@gmail.com>
2021-03-02 17:09:31 -0300

    Fix Brush of size == 1, fix OperationToolbar slide min_value

--- src/editor/selection/Brush.gd
+++ src/editor/selection/Brush.gd
@@ -14,11 +14,14 @@ func get_coordinates(position: Vector2, bounds_size: Vector2) -> PoolVector2Arra
 static func get_brush_coordinates(position: Vector2, bounds_size: Vector2, format: int, size: int) -> PoolVector2Array:
 	var array = PoolVector2Array()
 	var bounds = Rect2(Vector2.ZERO, bounds_size)
-	var half_size = int(size / 2)
-	for x in range(-half_size, half_size + 1):
-		for y in range(-half_size, half_size + 1):
-			var delta = Vector2(x, y)
-			var pos = position + delta
-			if bounds.has_point(pos) and (format == Format.SQUARE or delta.length() < half_size):
-				array.append(pos)
+	if size <= 1:
+		array.append(position)
+	else:
+		var half_size = int(size / 2)
+		for x in range(-half_size, half_size + 1):
+			for y in range(-half_size, half_size + 1):
+				var delta = Vector2(x, y)
+				var pos = position + delta
+				if bounds.has_point(pos) and (format == Format.SQUARE or delta.length() < half_size):
+					array.append(pos)
 	return array
