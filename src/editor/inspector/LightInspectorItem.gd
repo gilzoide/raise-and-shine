71ba0f91fdc37f9761190ce7c5a60d8003b0b055
gilzoide <gilzoide@gmail.com>
2021-03-02 22:46:11 -0300

    Fix light signal connections

--- src/editor/inspector/LightInspectorItem.gd
+++ src/editor/inspector/LightInspectorItem.gd
@@ -7,19 +7,21 @@ var light: Node
 
 func _ready() -> void:
 	assert(light != null, "FIXME!!!")
-	update_color_picker()
-	update_energy_slider_value()
-	light.connect("color_changed", self, "update_color_picker")
-	light.connect("energy_changed", self, "update_energy_slider_value")
+	update_color_picker(light.get_light_color())
+	update_energy_slider_value(light.get_light_energy())
+	if light.has_signal("color_changed"):
+		var _err = light.connect("color_changed", self, "update_color_picker")
+	if light.has_signal("energy_changed"):
+		var _err = light.connect("energy_changed", self, "update_energy_slider_value")
 
 func set_name_index(index: int):
 	title.text = "Ambient" if index < 0 else "Light %d" % index
 
-func update_energy_slider_value() -> void:
-	energy_slider.value = light.get_light_energy()
+func update_energy_slider_value(value: float) -> void:
+	energy_slider.value = value
 
-func update_color_picker() -> void:
-	color_picker.color = light.get_light_color()
+func update_color_picker(color: Color) -> void:
+	color_picker.color = color
 
 func _on_EnergySlider_value_changed(value: float) -> void:
 	light.set_light_energy(value)
