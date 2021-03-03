eea3076c9af140b13da59a7ddd3e2c3dbc16dfd6
gilzoide <gilzoide@gmail.com>
2021-03-03 18:58:41 -0300

    WIP Add brush direction

--- src/editor/selection/Brush.gd
+++ src/editor/selection/Brush.gd
@@ -15,32 +15,45 @@ enum Easing {
 
 export(Format) var format = Format.CIRCLE
 export(Easing) var easing = Easing.FLAT
+export(float) var direction = NAN
 export(int) var size: int = 8
 
 func get_coordinates(position: Vector2, bounds_size: Vector2) -> PoolVector3Array:
-	return get_brush_coordinates(position, bounds_size, format, size, easing)
+	return get_brush_coordinates(position, bounds_size, size)
 
 func get_affected_coordinates(position: Vector2, bounds_size: Vector2) -> PoolVector3Array:
-	return get_brush_coordinates(position, bounds_size, format, size + 2, easing)
+	return get_brush_coordinates(position, bounds_size, size + 2)
 
-static func get_brush_coordinates(position: Vector2, bounds_size: Vector2, format_: int, size_: int, easing_: int) -> PoolVector3Array:
+func get_brush_coordinates(position: Vector2, bounds_size: Vector2, size_: float) -> PoolVector3Array:
 	var array = PoolVector3Array()
 	var bounds = Rect2(Vector2.ZERO, bounds_size - Vector2.ONE)
 	if size_ <= 1:
 		array.append(Vector3(position.x, position.y, 0))
 	else:
-		var ease_param = easing_to_param(easing_)
+		var ease_param = easing_to_param(easing)
 		var half_size = int(size_ * 0.5)
 		for x in range(-half_size, half_size + 1):
 			for y in range(-half_size, half_size + 1):
 				var delta = Vector2(x, y)
 				var pos = position + delta
-				var pos_distance = delta.length()
-				if bounds.has_point(pos) and (format_ == Format.SQUARE or pos_distance < half_size):
-					var z = clamp(1 - ease(pos_distance / half_size, ease_param), 0, 1)
+				var normalized_delta = delta / half_size
+				if bounds.has_point(pos) and (format == Format.SQUARE or normalized_delta.length_squared() <= 1.0):
+					var z
+					if is_nan(direction):
+						z = get_brush_depth(normalized_delta.length(), ease_param)
+					else:
+						z = get_brush_direction_depth(normalized_delta, ease_param, direction)
 					array.append(Vector3(pos.x, pos.y, z))
 	return array
 
+
+static func get_brush_direction_depth(normalized_delta: Vector2, ease_param: float, direction_: float) -> float:
+	var delta_factor = -cos(direction_ - normalized_delta.angle())
+	var percent_from_highest = delta_factor * normalized_delta.length() * 0.5 + 0.5
+	return get_brush_depth(percent_from_highest, ease_param)
+
+static func get_brush_depth(percent_from_highest: float, ease_param: float) -> float:
+	return 1 - ease(percent_from_highest, ease_param)
 
 static func easing_to_param(easing_: int) -> float:
 	if easing_ == Easing.LINEAR:
@@ -53,4 +66,4 @@ static func easing_to_param(easing_: int) -> float:
 		return -2.0
 	else:  # FLAT
 		return 0.0
-	
+
