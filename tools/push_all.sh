3c9c362fb80c9c1772236eb828da23db86b9b207
gilzoide <gilzoide@gmail.com>
2021-03-11 23:04:03 -0300

    Remove "--if-changed" from butler calls

--- tools/push_all.sh
+++ tools/push_all.sh
@@ -3,9 +3,9 @@
 script_root=$(dirname $(realpath $0))
 project_root="$script_root/.."
 
-butler push --if-changed "$project_root/build/RaiseAndShine_web.zip"      gilzoide/raise-and-shine:web
-butler push --if-changed "$project_root/build/RaiseAndShine_win32.zip"    gilzoide/raise-and-shine:win32
-butler push --if-changed "$project_root/build/RaiseAndShine_linux32.zip"  gilzoide/raise-and-shine:linux32
-butler push --if-changed "$project_root/build/RaiseAndShine_linux64.zip"  gilzoide/raise-and-shine:linux64
-butler push --if-changed "$project_root/build/RaiseAndShine_osx.zip"      gilzoide/raise-and-shine:osx
+butler push "$project_root/build/RaiseAndShine_web.zip"      gilzoide/raise-and-shine:web
+butler push "$project_root/build/RaiseAndShine_win32.zip"    gilzoide/raise-and-shine:win32
+butler push "$project_root/build/RaiseAndShine_linux32.zip"  gilzoide/raise-and-shine:linux32
+butler push "$project_root/build/RaiseAndShine_linux64.zip"  gilzoide/raise-and-shine:linux64
+butler push "$project_root/build/RaiseAndShine_osx.zip"      gilzoide/raise-and-shine:osx
 
