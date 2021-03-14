84657047815bb6db7ca0ce454d6ea59106190ace
gilzoide <gilzoide@gmail.com>
2021-03-14 16:42:51 -0300

    Update push_all.sh to use commands relative to project root

--- tools/push_all.sh
+++ tools/push_all.sh
@@ -3,9 +3,12 @@
 script_root=$(dirname $(realpath $0))
 project_root="$script_root/.."
 
-butler push "$project_root/build/RaiseAndShine_web.zip"      gilzoide/raise-and-shine:web
-butler push "$project_root/build/RaiseAndShine_win32.zip"    gilzoide/raise-and-shine:win32
-butler push "$project_root/build/RaiseAndShine_linux32.zip"  gilzoide/raise-and-shine:linux32
-butler push "$project_root/build/RaiseAndShine_linux64.zip"  gilzoide/raise-and-shine:linux64
-butler push "$project_root/build/RaiseAndShine_osx.zip"      gilzoide/raise-and-shine:osx
+pushd "$project_root"
 
+butler push "build/RaiseAndShine_web.zip"      gilzoide/raise-and-shine:web
+butler push "build/RaiseAndShine_win32.zip"    gilzoide/raise-and-shine:win32
+butler push "build/RaiseAndShine_linux32.zip"  gilzoide/raise-and-shine:linux32
+butler push "build/RaiseAndShine_linux64.zip"  gilzoide/raise-and-shine:linux64
+butler push "build/RaiseAndShine_osx.zip"      gilzoide/raise-and-shine:osx
+
+popd
