e7a8ad224c87eb5ba5a28f2d2a307f6a930fb8a4
gilzoide <gilzoide@gmail.com>
2021-03-14 16:41:21 -0300

    Apply height operations with pixel center in mind

--- src/editor/operation/HeightOperation.gd
+++ src/editor/operation/HeightOperation.gd
@@ -21,6 +21,7 @@ enum Easing {
 }
 
 const RADIAL_DIRECTION = NAN
+const PIXEL_OFFSET = Vector2(0.5, 0.5)
 
 export(Operation) var type = Operation.INCREASE_BY
 export(float) var amount = 0.1
@@ -45,7 +46,7 @@ func offset_by(heightmap: HeightMapData, mask: BitMap, rect: Rect2, value: float
 				if easing == Easing.FLAT:
 					depth = 1.0
 				else:
-					var normalized_delta = (v - center) / half_size
+					var normalized_delta = (v + PIXEL_OFFSET - center) / half_size
 					if is_radial_direction(direction):
 						depth = get_brush_depth(normalized_delta.length(), ease_func)
 					else:
