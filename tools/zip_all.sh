814a3aeb2ce044bc6fd8cfdc752782137398973a
gilzoide <gilzoide@gmail.com>
2021-03-18 15:52:39 -0300

    Fix zip_all to include GDNative libraries

--- tools/zip_all.sh
+++ tools/zip_all.sh
@@ -4,9 +4,9 @@ script_root=$(dirname $(realpath $0))
 project_root="$script_root/.."
 pushd "$project_root/build"
 
-zip RaiseAndShine_win32 -j RaiseAndShine_win32.{exe,pck}
-zip RaiseAndShine_linux32 -j RaiseAndShine_linux32.{x86,pck}
-zip RaiseAndShine_linux64 -j RaiseAndShine_linux64.{x86_64,pck}
+zip RaiseAndShine_win32 -j RaiseAndShine_win32.{exe,pck} height_algorithm_win32.dll
+zip RaiseAndShine_linux32 -j RaiseAndShine_linux32.{x86,pck} height_algorithm_linux32.so
+zip RaiseAndShine_linux64 -j RaiseAndShine_linux64.{x86_64,pck} height_algorithm_linux64.so
 zip RaiseAndShine_web -j web/*
 
 popd
