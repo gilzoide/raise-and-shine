5e00befccc57c07bb77e379004806481eed8dc63
gilzoide <gilzoide@gmail.com>
2021-03-19 17:06:44 -0300

    Add Windows x64 build

--- tools/zip_all.sh
+++ tools/zip_all.sh
@@ -5,6 +5,7 @@ project_root="$script_root/.."
 pushd "$project_root/build"
 
 zip RaiseAndShine_win32 -j RaiseAndShine_win32.{exe,pck} height_algorithm_win32.dll
+zip RaiseAndShine_win64 -j RaiseAndShine_win64.{exe,pck} height_algorithm_win64.dll
 zip RaiseAndShine_linux32 -j RaiseAndShine_linux32.{x86,pck} height_algorithm_linux32.so
 zip RaiseAndShine_linux64 -j RaiseAndShine_linux64.{x86_64,pck} height_algorithm_linux64.so
 zip RaiseAndShine_web -j web/*
