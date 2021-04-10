d7dd2ad2875627172a6ad360938caec341b6c07f
gilzoide <gilzoide@gmail.com>
2021-04-10 11:45:20 -0300

    Add Build 2 changelog

--- src/changelog/ChangelogVersion.gd
+++ src/changelog/ChangelogVersion.gd
@@ -4,15 +4,15 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 extends Resource
 
-const CURRENT_VERSION = 1
+const CURRENT_VERSION = 2
 const DATE_FMT = "{year}-{month}-{day}"
-const BBCODE_FMT = """
-[u][b]Build {build_number}[/b]  ({date})[/u]
-{description}
-{added}{changed}{fixed}{removed}
+const BBCODE_FMT = """[u][b]Build {build_number}[/b]  ({date})[/u]
+{content}
+
+
 """
 
-export(int) var build_number = CURRENT_VERSION
+export(int) var build_number = 0
 export(String) var date = DATE_FMT.format(OS.get_date())
 export(String, MULTILINE) var description = ""
 export(PoolStringArray) var added = PoolStringArray()
@@ -22,16 +22,20 @@ export(PoolStringArray) var removed = PoolStringArray()
 
 
 func generate_bbcode() -> String:
-	var added_text = "\nAdded:\n• %s" % added.join('\n• ') if not added.empty() else ""
-	var changed_text = "\nChanged:\n%s" % changed.join('\n') if not changed.empty() else ""
-	var fixed_text = "\nFixed:\n%s" % fixed.join('\n') if not fixed.empty() else ""
-	var removed_text = "\nRemoved:\n%s" % removed.join('\n') if not removed.empty() else ""
+	var content = PoolStringArray()
+	if not description.empty():
+		content.append(description)
+		content.append("")
+	if not added.empty():
+		content.append("[u]Added:[/u]\n• %s" % added.join("\n• "))
+	if not changed.empty():
+		content.append("[u]Changed:[/u]\n• %s" % changed.join("\n• "))
+	if not fixed.empty():
+		content.append("[u]Fixed:[/u]\n• %s" % fixed.join("\n• "))
+	if not removed.empty():
+		content.append("[u]Removed:[/u]\n• %s" % removed.join("\n• "))
 	return BBCODE_FMT.format({
-		'build_number': build_number,
-		'date': date,
-		'description': description,
-		'added': added_text,
-		'changed': changed_text,
-		'fixed': fixed_text,
-		'removed': removed_text,
+		build_number = build_number,
+		date = date,
+		content = content.join("\n"),
 	})
