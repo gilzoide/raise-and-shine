a23fe3a2ac0571c7b02ba3d8ca740d60f8dde481
gilzoide <gilzoide@gmail.com>
2021-04-15 11:10:50 -0300

    Add depth controls to brush inspector

--- src/editor/brush/BrushInspector.gd
+++ src/editor/brush/BrushInspector.gd
@@ -9,8 +9,19 @@ export(Resource) var brush = preload("res://editor/brush/ActiveBrush.tres")
 onready var _size_spinbox = $SizeHeader/SpinBox
 onready var _size_slider = $SizeSlider
 
+onready var _depth_spinbox = $DepthHeader/SpinBox
+onready var _depth_slider = $DepthSlider
+
 
 func _ready() -> void:
 	_size_spinbox.share(_size_slider)
 	_size_spinbox.value = brush.size
 	_size_spinbox.connect("value_changed", brush, "set_size")
+	
+	_depth_spinbox.share(_depth_slider)
+	_depth_spinbox.value = int(brush.depth * 100)
+	_depth_spinbox.connect("value_changed", self, "_on_depth_value_changed")
+
+
+func _on_depth_value_changed(value) -> void:
+	brush.depth = value / 100.0
