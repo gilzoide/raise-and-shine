641d5ad36d9fb10252cd19ee47fb8234bd62cfff
gilzoide <gilzoide@gmail.com>
2021-04-16 15:34:40 -0300

    Add reset brush property initial value with right mouse button

--- src/editor/brush/BrushPropertyEditor.gd
+++ src/editor/brush/BrushPropertyEditor.gd
@@ -8,21 +8,25 @@ extends Control
 export(Resource) var resource = preload("res://editor/brush/ActiveBrush.tres")
 export(String) var property := "size" setget set_property
 
+var _initial_value
 onready var _label = $Header/Label
 onready var _spinbox = $Header/SpinBox
 onready var _slider = $Slider
 
 
 func _ready() -> void:
+	_initial_value = resource.get(property)
 	_label.text = property.capitalize()
 	_spinbox.share(_slider)
-	_spinbox.value = resource.get(property)
+	_spinbox.value = _initial_value
 	_spinbox.connect("value_changed", self, "_on_value_changed")
 
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == BUTTON_WHEEL_UP:
+		if event.button_index == BUTTON_RIGHT:
+			_spinbox.value = _initial_value
+		elif event.button_index == BUTTON_WHEEL_UP:
 			_spinbox.value += _spinbox.step
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			_spinbox.value -= _spinbox.step
