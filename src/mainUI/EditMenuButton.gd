0db0a6b99dea12ddc3e6ac3dc3146651e82a4e49
gilzoide <gilzoide@gmail.com>
2021-04-17 12:08:14 -0300

    Remove selection related options from Edit menu button

--- src/mainUI/EditMenuButton.gd
+++ src/mainUI/EditMenuButton.gd
@@ -8,10 +8,6 @@ enum {
 	UNDO,
 	REDO,
 	_SEPARATOR_0,
-	SELECTION_CLEAR,
-	SELECTION_ALL,
-	SELECTION_INVERT,
-	_SEPARATOR_1,
 	RESIZE_MAPS,
 }
 
@@ -31,20 +27,13 @@ func _ready() -> void:
 	menu_popup.set_item_shortcut(REDO, preload("res://shortcuts/RedoShortcut.tres"))
 	
 	menu_popup.add_separator()
-	menu_popup.add_item("Clear selection", SELECTION_CLEAR)
-	menu_popup.set_item_shortcut(SELECTION_CLEAR, preload("res://shortcuts/SelectionClear_shortcut.tres"))
-	menu_popup.add_item("Select all", SELECTION_ALL)
-	menu_popup.set_item_shortcut(SELECTION_ALL, preload("res://shortcuts/SelectionAll_shortcut.tres"))
-	menu_popup.add_item("Invert selection", SELECTION_INVERT)
-	menu_popup.set_item_shortcut(SELECTION_INVERT, preload("res://shortcuts/SelectionInvert_shortcut.tres"))
-	menu_popup.connect("about_to_show", self, "_on_menu_popup_about_to_show")
-	menu_popup.connect("id_pressed", self, "_on_menu_popup_id_pressed")
-	
-	menu_popup.add_separator()
 	menu_popup.add_item("Resize maps", RESIZE_MAPS)
 	menu_popup.set_item_tooltip(RESIZE_MAPS, """
 	Resize height and normal maps, scaling current content.
 	""")
+	
+	menu_popup.connect("about_to_show", self, "_on_menu_popup_about_to_show")
+	menu_popup.connect("id_pressed", self, "_on_menu_popup_id_pressed")
 
 
 func call_undo() -> void:
@@ -60,12 +49,6 @@ func _on_menu_popup_id_pressed(id: int) -> void:
 		call_undo()
 	elif id == REDO:
 		call_redo()
-	elif id == SELECTION_CLEAR:
-		selection.clear(false)
-	elif id == SELECTION_ALL:
-		selection.clear(true)
-	elif id == SELECTION_INVERT:
-		selection.invert()
 	elif id == RESIZE_MAPS:
 		if _size_picker_popup == null:
 			_size_picker_popup = load("res://mainUI/SizePickerPopup.tscn").instance()
