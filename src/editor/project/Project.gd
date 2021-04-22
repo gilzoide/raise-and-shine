6f4c02c89cea319990aecc7df659dbc66adedb2e
gilzoide <gilzoide@gmail.com>
2021-04-22 19:39:03 -0300

    Fix generate new Image for heightmap when opening new texture

--- src/editor/project/Project.gd
+++ src/editor/project/Project.gd
@@ -76,9 +76,10 @@ func on_project_dialog_image(value: Image, path: String = "") -> void:
 	last_loaded_filename = path.get_file().get_basename()
 	set_albedo_image(value)
 	var new_size = value.get_size()
-	height_image.create(new_size.x, new_size.y, false, MapTypes.HEIGHT_IMAGE_FORMAT)
-	set_height_image(height_image, true)
-	history.push_revision(height_image)
+	var new_height_image = Image.new()
+	new_height_image.create(new_size.x, new_size.y, false, MapTypes.HEIGHT_IMAGE_FORMAT)
+	set_height_image(new_height_image, true)
+	history.push_revision(new_height_image)
 
 
 func set_albedo_image(value: Image, _path: String = "") -> void:
