93bd13c3812297840c4d1bb41767c10184a7f769
gilzoide <gilzoide@gmail.com>
2021-04-30 18:58:41 -0300

    Add changelog for build 4

--- src/help/OnboardingTabNews.gd
+++ src/help/OnboardingTabNews.gd
@@ -11,7 +11,7 @@ onready var _richtext = $ScrollContainer/NewsLabel
 
 func _ready() -> void:
 	var new_text_array = PoolStringArray()
-	for v in versions:
-		new_text_array.append(v.generate_bbcode())
+	for i in range(versions.size() - 1, -1, -1):
+		new_text_array.append(versions[i].generate_bbcode())
 	var new_text = new_text_array.join("\n\n\n").replace("[url", "[color=#ff8588][url").replace("[/url]", "[/url][/color]")
 	_richtext.bbcode_text = new_text
